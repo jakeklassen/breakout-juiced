5a0b22a8072f395929afa8d52d8fec6f79cff5f2
Jake Klassen <jklassendev@gmail.com>
2019-08-27 08:23:57 -0400

    saving work

--- src/components/BoxCollider2d.ts
+++ src/components/BoxCollider2d.ts
@@ -1,4 +1,5 @@
 import { Component } from '@jakeklassen/ecs';
+import { Vector2d } from '../lib/Vector2d';
 
 export class BoxCollider2d extends Component {
   constructor(
@@ -16,6 +17,13 @@ export class BoxCollider2d extends Component {
 
   get right() {
     return this.x + this.width;
+  }
+
+  get center() {
+    return new Vector2d(
+      this.right - this.width / 2,
+      this.bottom - this.height / 2,
+    );
   }
 
   get top() {
--- src/systems/BallBrickCollisionSystem.ts
+++ src/systems/BallBrickCollisionSystem.ts
@@ -1,4 +1,5 @@
 import { System, World } from '@jakeklassen/ecs';
+import SAT from 'sat';
 import { BoxCollider2d } from '../components/BoxCollider2d';
 import { BallTag } from '../components/BallTag';
 import { BrickTag } from '../components/BrickTag';
@@ -6,6 +7,17 @@ import { intersects } from '../lib/aabb';
 import { Velocity2d } from '../components/Velocity2d';
 import { Transform } from '../components/Transform';
 import { clamp } from '../lib/math';
+import { Vector2d } from '../lib/Vector2d';
+
+const boxCollider2dToSATBox = (collider: BoxCollider2d) =>
+  new SAT.Box(
+    new SAT.Vector(collider.x, collider.y),
+    collider.width,
+    collider.height,
+  );
+
+const vec2Distance = (v1: Vector2d, v2: Vector2d) =>
+  Math.abs(Math.sqrt((v2.x - v1.x) ** 2 + (v2.y - v1.y) ** 2));
 
 export class BallBrickCollisionSystem extends System {
   update(world: World, dt: number): void {
@@ -15,62 +27,67 @@ export class BallBrickCollisionSystem extends System {
     const ballCollider = ballComponents.get<BoxCollider2d>(BoxCollider2d)!;
     const ballVelocity = ballComponents.get<Velocity2d>(Velocity2d)!;
 
+    const ballBox = boxCollider2dToSATBox(ballCollider);
+
     // Query the world for bricks
     const bricks = world.view(BrickTag);
 
-    // Solve for Y
+    const collisions: [SAT.Response, BoxCollider2d][] = [];
+
     for (const [brick, components] of bricks) {
       const brickCollider = components.get<BoxCollider2d>(BoxCollider2d)!;
 
-      if (
-        intersects(ballCollider, brickCollider) &&
-        ballTransform.position.x ===
-          clamp(
-            ballTransform.position.x,
-            brickCollider.left,
-            brickCollider.right,
-          )
-      ) {
-        console.log('y');
+      const brickBox = boxCollider2dToSATBox(brickCollider);
+      const response = new SAT.Response();
+      const collided = SAT.testPolygonPolygon(
+        ballBox.toPolygon(),
+        brickBox.toPolygon(),
+        response,
+      );
 
-        // Snap to bottom of collider
-        if (ballVelocity.y < 0) {
-          ballCollider.y = ballTransform.position.y = brickCollider.bottom;
-        } else if (ballVelocity.y > 0) {
-          ballCollider.y = ballTransform.position.y =
-            brickCollider.top - brickCollider.height;
-        }
-
-        ballVelocity.flipY();
-        return;
+      if (collided) {
+        collisions.push([response, brickCollider]);
       }
     }
 
-    // Solve for X
-    for (const [brick, components] of bricks) {
-      const brickCollider = components.get<BoxCollider2d>(BoxCollider2d)!;
-      if (
-        intersects(ballCollider, brickCollider) &&
-        ballTransform.position.y ===
-          clamp(
-            ballTransform.position.y,
-            brickCollider.top,
-            brickCollider.bottom,
-          )
-      ) {
-        console.log('x');
+    if (collisions.length === 0) {
+      return;
+    }
 
-        // Snap to bottom of collider
-        if (ballVelocity.x < 0) {
-          ballCollider.x = ballTransform.position.x = brickCollider.right;
-        } else if (ballVelocity.x > 0) {
-          ballCollider.x = ballTransform.position.x =
-            brickCollider.left - brickCollider.width;
-        }
-
-        ballVelocity.flipX();
-        return;
+    const closestCollision = collisions.reduce((closest, collision) => {
+      if (collision[0].overlap < closest[0].overlap) {
+        closest = collision;
       }
+
+      return closest;
+    }, collisions[0]);
+
+    const [response, brickCollider] = closestCollision;
+
+    if (Math.abs(response.overlapN.y) !== 0) {
+      // if (ballVelocity.y > 0) {
+      //   ballTransform.position.y = ballCollider.y =
+      //     brickCollider.top - ballCollider.height;
+      // } else if (ballVelocity.y < 0) {
+      //   ballTransform.position.y = ballCollider.y = brickCollider.bottom;
+      // }
+
+      ballTransform.position.y = ballCollider.y =
+        ballCollider.y - response.overlapV.y;
+
+      ballVelocity.flipY();
+    } else if (Math.abs(response.overlapN.x) !== 0) {
+      // if (ballVelocity.x > 0) {
+      //   ballTransform.position.x = ballCollider.x =
+      //     brickCollider.left - ballCollider.width;
+      // } else if (ballVelocity.x < 0) {
+      //   ballTransform.position.x = ballCollider.x = brickCollider.right;
+      // }
+
+      ballTransform.position.x = ballCollider.x =
+        ballCollider.x - response.overlapV.x;
+
+      ballVelocity.flipX();
     }
   }
 }
